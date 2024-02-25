4004c8398be7dac5f2ab416403c00a5d4bc80720
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-25 17:15:34 -0500

    Create UsuarioModelo.java

---
+++ productor_java_rabitmq/src/main/java/com/example/productor_java_rabitmq/model/UsuarioModelo.java
@@ -0,0 +1,12 @@
+package com.example.productor_java_rabitmq.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class UsuarioModelo {
+    private String nombre, apellido, identificacion, correo, ciudad_residencia,direccion;
+    private String fecha_nacimiento;
+    
+}
