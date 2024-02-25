9442ba660b2bb91986b5ef8a0b88ddb30861116e
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-25 17:41:05 -0500

    Consumidor Postgres

---
+++ consumidor_postgres/src/main/java/com/example/consumidor_postgres/ConsumidorPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumidor_postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumidorPostgresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumidorPostgresApplication.class, args);
+    }
+
+}
---
+++ consumidor_postgres/src/test/java/com/example/consumidor_postgres/ConsumidorPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumidor_postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumidorPostgresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
