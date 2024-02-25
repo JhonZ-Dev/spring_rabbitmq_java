09ff8a04ceaee617034c206430ab08657928fcf2
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-25 17:25:35 -0500

    Update ConfigRabbit.java

--- productor_java_rabitmq/src/main/java/com/example/productor_java_rabitmq/configuracion/ConfigRabbit.java
+++ productor_java_rabitmq/src/main/java/com/example/productor_java_rabitmq/configuracion/ConfigRabbit.java
@@ -7,5 +7,8 @@ public class ConfigRabbit {
     //primero definir las colas
     public static final String cola_sd="Santo_Domingo";
     public static final String cola_otras_ciudad="Otras_ciudades";
-    
+
+    //segundo definir las rutas
+    public static final String ROUTING_A="santo-domingo";
+    public static final String ROUTING_B = "otra-ciudad";
 }
