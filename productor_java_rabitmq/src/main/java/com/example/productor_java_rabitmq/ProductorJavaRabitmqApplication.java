dfac8e799062210a8d38297fcd1f50c3fe5248a4
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-25 17:13:41 -0500

    Proyecto Init

---
+++ productor_java_rabitmq/src/main/java/com/example/productor_java_rabitmq/ProductorJavaRabitmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.productor_java_rabitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductorJavaRabitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductorJavaRabitmqApplication.class, args);
+    }
+
+}
---
+++ productor_java_rabitmq/src/test/java/com/example/productor_java_rabitmq/ProductorJavaRabitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productor_java_rabitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductorJavaRabitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
