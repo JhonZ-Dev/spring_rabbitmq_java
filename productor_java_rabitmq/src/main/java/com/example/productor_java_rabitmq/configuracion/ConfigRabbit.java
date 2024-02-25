1b143a1cfdd731d9188d39141db215c1442332c1
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-25 17:18:06 -0500

    Update ConfigRabbit.java

--- productor_java_rabitmq/src/main/java/com/example/productor_java_rabitmq/configuracion/ConfigRabbit.java
+++ productor_java_rabitmq/src/main/java/com/example/productor_java_rabitmq/configuracion/ConfigRabbit.java
@@ -4,4 +4,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConfigRabbit {
+    //primero definir las colas
+    public static final String cola_sd="Santo_Domingo";
+    public static final String cola_otras_ciudad="Otras_ciudades";
+    
 }
