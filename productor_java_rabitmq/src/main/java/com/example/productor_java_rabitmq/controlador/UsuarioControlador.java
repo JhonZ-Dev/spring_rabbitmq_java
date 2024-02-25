f689ed5f0b69f13e49771531817a6a0f61205e4c
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-25 17:36:43 -0500

    Update UsuarioControlador.java

--- productor_java_rabitmq/src/main/java/com/example/productor_java_rabitmq/controlador/UsuarioControlador.java
+++ productor_java_rabitmq/src/main/java/com/example/productor_java_rabitmq/controlador/UsuarioControlador.java
@@ -1,8 +1,11 @@
 package com.example.productor_java_rabitmq.controlador;
 
+import com.example.productor_java_rabitmq.model.UsuarioModelo;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +16,16 @@ public class UsuarioControlador {
     private RabbitTemplate rabbitTemplate;
     @Autowired
     private DirectExchange exchange;
-    
+
+    @PostMapping("/productor")
+    public String usuarioProductor(@RequestBody UsuarioModelo usuario){
+        String ciudad = usuario.getCiudad_residencia();
+        if ("Santo Domingo".equalsIgnoreCase(ciudad)) {
+            rabbitTemplate.convertAndSend(exchange.getName(), "santo-domingo", usuario);
+            return "Usuarios enviado a la cola de Santo Domingo";
+        } else {
+            rabbitTemplate.convertAndSend(exchange.getName(), "otra-ciudad", usuario);
+            return "Usuarios enviado a la cola de Otra Ciudad";
+        }
+    }
 }
