4b44b31569f6d41e761b63fc23ae952d9e05d951
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-25 17:49:32 -0500

    Update MessageControlador.java

--- consumidor_postgres/src/main/java/com/example/consumidor_postgres/controlador/MessageControlador.java
+++ consumidor_postgres/src/main/java/com/example/consumidor_postgres/controlador/MessageControlador.java
@@ -1,7 +1,9 @@
 package com.example.consumidor_postgres.controlador;
 
+import com.example.consumidor_postgres.modelo.Message;
 import com.example.consumidor_postgres.servicio.ConsumerService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +14,10 @@ public class MessageControlador {
     @Autowired
     public ConsumerService consumerService;
 
-    
+    @RabbitListener(queues = "Santo-Domingo")
+    private void receive(Message message){
+        log.info("Mensaje del productor ->{}", message);
+
+        consumerService.guardar(message);
+    }
 }
